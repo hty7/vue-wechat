c12eeb34d5642e97a2923fe379be6b8b15089e50
YJ <1553937164@qq.com>
2018-08-08 16:51:54 +0800

    聊天气泡

--- src/store/modules/wx.js
+++ src/store/modules/wx.js
@@ -4,7 +4,7 @@ import {getLocalStorage, setLocalStorage} from '@/utils/utils'
 const state = {
   isWXLogin: JSON.parse(getLocalStorage('isWXLogin')), // 用户微信登陆状态
   hasCheckLoading: 0, // 心跳是否正常
-  loginWxUserInfo: JSON.parse(getLocalStorage('isWXLogin')), // 微信用户信息
+  loginWxUserInfo: JSON.parse(getLocalStorage('loginWxuserInfo')), // 微信用户信息
   wxUserTab: [], // 用户标签
   chatLogShow: false,
   redirectUriType: JSON.parse(getLocalStorage('redirectUriType')), // 重定向地址
@@ -13,9 +13,24 @@ const state = {
   userMemberList: [], // 微信用户通讯录列表
   addressBookIndex: {}, // 微信用户通讯录索引
   chatUserList: [], // 侧边栏聊天客户
-  activeUser: {}, // 高亮用户
+  activeUser: {
+    HeadImgUrl: '/static/images/logo.png'
+  }, // 高亮用户
   activeIndex: 0, // 聊天高亮节点
-  activeMessageList: [], // 当前聊天用户聊天记录
+  activeMessageList: [
+    {
+      Content: 'dddddd',
+      MsgType: 1,
+      FromUserName: '@03acd75b7d9d33002c68fa7f4be7ebe1a8f403d62480a0de19976bbf5929f75a',
+      ToUserName: '@fgghhhhh766'
+    },
+    {
+      Content: 'dd会随机发货v决定能否vdddd',
+      MsgType: 1,
+      FromUserName: '@fgghhhhh766',
+      ToUserName: '@03acd75b7d9d33002c68fa7f4be7ebe1a8f403d62480a0de19976bbf5929f75a'
+    }
+  ], // 当前聊天用户聊天记录
   userChatLog: {}, // 用户聊天记录列表
   startCheckWebSync: 0, // 启动微信心跳
   hasWxUserList: {}
--- src/view/wechat/children/wxChatContent.vue
+++ src/view/wechat/children/wxChatContent.vue
@@ -4,7 +4,28 @@
       <wx-chat-header></wx-chat-header>
     </div>
     <div class="chat-session-body-bd">
-      ddd
+      <div class="message_empty" v-if="!activeMessageList.length">暂无新消息</div>
+      <div class="message" v-else ref="chatSessionMessage">
+        <div v-for="(item, index) in activeMessageList" :key="index">
+          <div class="clearfix">
+            <div :class="['message-item', item.FromUserName !== loginWxUserInfo.User.UserName?'you':'me']">
+              <img :src="baseURL + loginWxUserInfo.User.HeadImgUrl" class="avatar" v-if="item.FromUserName === loginWxUserInfo.User.UserName">
+              <img :src="activeUser.HeadImgUrl" class="avatar" alt="" v-else>
+              <div class="chatcontent">
+                <div class="bubble" :style="{backgroundColor: item.FromUserName !== loginWxUserInfo.User.UserName?'#fff':'#3b7abe', color: item.FromUserName !== loginWxUserInfo.User.UserName?'#000':'#fff'}">
+                  <div v-show="item.recall" class="recall">信息已被撤回</div>
+                  <!-- 1 文字 3 图片 34语音 47表情 -->
+                  <div class="bubble-text" v-show="item.MsgType === 1">
+                    <div class="plain">
+                      <span v-html="item.Content"></span>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
     <div class="chat-session-body-ft">
       <wx-chat-send-box></wx-chat-send-box>
@@ -24,7 +45,10 @@ export default {
     userCardShow: false
   }),
   computed: {
-    ...mapGetters(['isWXLogin'])
+    ...mapGetters(['isWXLogin', 'activeMessageList', 'loginWxUserInfo', 'redirectUriType', 'activeUser']),
+    baseURL () {
+      return this.redirectUriType ? '/wx2' : '/wx1'
+    }
   },
   methods: {
     ...mapActions(['getWxUserInfo', 'updateWxUserMember']),
@@ -48,6 +72,114 @@ export default {
     padding: 20px 0;
     overflow-y: auto;
     overflow-x: hidden;
+    .message_empty {
+      text-align: center;
+      padding: 128px;
+      color: #ccc;
+      font-size: 13px;
+    }
+    .message {
+      color: #2f2f2f;
+      .me {
+        float: right;
+        text-align: right;
+        clear: right;
+        .voice {
+          .iconfont {
+            float: right;
+          }
+        }
+      }
+      .me {
+        .bubble {
+          &:after {
+            content: '';
+            position: absolute;
+            right: -10px;
+            top: 5px;
+            border-width: 0px 0 13px 12px;
+            border-style: solid;
+            border-color: transparent transparent transparent #3b7abe;
+          }
+        }
+        .avatar{
+          float: right;
+        }
+      }
+      .you {
+        .bubble {
+          &:before {
+            content: '';
+            position: absolute;
+            left: -10px;
+            top: 5px;
+            border-width: 0px 12px 13px 0px;
+            border-style: solid;
+            border-color: transparent #ffffff transparent transparent;
+          }
+        }
+      }
+      &-item {
+        width: 90%;
+        padding: 0 28px;
+        margin-bottom: 16px;
+        float: left;
+        .avatar {
+          width: 40px;
+          height: 40px;
+          border-radius: 50%;
+          border: 4px #fff solid;
+          background: #fff;
+          float: left;
+          margin: 5px 0;
+          cursor: pointer;
+        }
+        .chatcontent {
+          overflow: hidden;
+          .bubble {
+            max-width: 500px;
+            min-height: 40px;
+            display: inline-block;
+            vertical-align: top;
+            position: relative;
+            text-align: left;
+            font-size: 14px;
+            border-radius: 5px;
+            margin: 5px 10px;
+            &-image {
+              cursor: pointer;
+              height: 120px;
+              .custom-emoji {
+                width: 60px;
+              }
+            }
+            &-text {
+              word-wrap: break-word;
+              word-break: break-all;
+              min-height: 25px;
+              .plain {
+                padding: 9px 13px;
+              }
+              .voice {
+                padding: 10px 13px;
+                width: 120px;
+                max-width: 300px;
+                cursor: pointer;
+                position: relative;
+              }
+            }
+            .recall {
+              position: absolute;
+              right: -110px;
+              top: 10px;
+              width: 100px;
+              color: #b3b3b3;
+              font-size: 12px;
+            }
+          }
+        }
+      }
+    }
   }
   &-ft {
     height: 118px;
